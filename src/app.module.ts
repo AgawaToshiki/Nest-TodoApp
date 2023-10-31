46b44e3eb696b6684206f50114c663685fc9d89c
Agawa <t.agawa1128ht@gmail.com>
2023-10-31 13:25:37 +0900

    TaskListモジュールの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TaskListController } from './task-list/task-list.controller';
+import { TaskListService } from './task-list/task-list.service';
+import { TaskListModule } from './task-list/task-list.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TaskListModule],
+  controllers: [AppController, TaskListController],
+  providers: [AppService, TaskListService],
 })
 export class AppModule {}
---
+++ src/task-list/task-list.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TaskListController } from "./task-list.controller"
+import { TaskListService } from "./task-list.service"
+
+@Module({
+    controllers: [TaskListController]
+})
+export class TaskListModule {}
