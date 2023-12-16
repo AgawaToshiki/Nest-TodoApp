04c22ee7c1e964e943ec3f2b7291865dd8cc0cd2
AgawaToshiki <t.agawa1128ht@gmail.com>
2023-12-16 22:59:05 +0900

    レスポンシブに対応

--- public/index.js
+++ public/index.js
@@ -1,24 +1,15 @@
-const deleteUser = document.getElementById('delete-user')
-deleteUser.addEventListener('click', async () => {
-  const confirmed = confirm('アカウントを削除しますか？')
-    if(confirmed){
-      await doDeleteUser();
-    }
+const toggleBtn = document.querySelector('.toggle-button');
+const dropBox = document.querySelector('.dropdown')
+const changeModalWindow = () => {
+  dropBox.classList.toggle('active');
+} 
+toggleBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  changeModalWindow();
 })
 
-async function doDeleteUser(){
-  try{
-    await fetch("/deleteUser", {
-      method: "POST",
-    }).then((response) => {
-      if(response.redirected){
-        const redirectUrl = response.url;
-        window.location.href = redirectUrl;
-      }else{
-        return response.json();
-      }
-    })
-  }catch(error){
-    console.error(error)
+document.documentElement.addEventListener('click', () => {
+  if(dropBox.classList.toggle('active')){
+    changeModalWindow();
   }
-}
+})
