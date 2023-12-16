535d7cc57bb41bb01225399bfabd16e301bab1a6
AgawaToshiki <t.agawa1128ht@gmail.com>
2023-12-16 23:56:08 +0900

    style修正

--- public/js/task.js
+++ public/js/task.js
@@ -22,3 +22,20 @@ async function doDeleteUser(){
     console.error(error)
   }
 }
+
+
+const toggleBtn = document.querySelector('.toggle-button');
+const dropBox = document.querySelector('.dropdown')
+const changeModalWindow = () => {
+  dropBox.classList.toggle('active');
+} 
+toggleBtn.addEventListener('click', (e) => {
+  e.stopPropagation();
+  changeModalWindow();
+})
+
+document.documentElement.addEventListener('click', () => {
+  if(dropBox.classList.toggle('active')){
+    changeModalWindow();
+  }
+})
